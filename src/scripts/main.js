a1d88e8513910fe3c0b1b25ff4700ba6ec46d22f
Samuel Silva dos Santos <akitadecumace@gmail.com>
2025-05-10 20:47:48 -0300

    corrigindo a data do evento

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
 AOS.init();
 
-const dataDoEvento = new Date("May 9, 2025 17:00:00");
+const dataDoEvento = new Date("May 31, 2025 17:00:00");
 const timeStampDoEvento = dataDoEvento.getTime();
 
 const contaAsHoras = setInterval(function() {
